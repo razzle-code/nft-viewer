74e20b763c60f9c96029ad01e2adc8a4204ffeaf
razzle-code <125223820+razzle-code@users.noreply.github.com>
2023-07-16 00:44:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -69,32 +69,81 @@ async function viewArt(tokenId) {
 }
 
 
-function drawDragonCurve(sequence, size) {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+function drawDragonCurve(dragonCurve, size) {
+  let x = 0,
+    y = 0,
+    direction = 0,
+    directions = [
+      [0, 1],
+      [1, 0],
+      [0, -1],
+      [-1, 0]
+    ];
+  let minX = Infinity,
+    maxX = -Infinity,
+    minY = Infinity,
+    maxY = -Infinity;
 
-    const scalingFactor = size / (2 * sequence.length);
-    let x = size / 2;
-    let y = size / 2;
-
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.beginPath();
-    ctx.moveTo(x, y);
-
-    for (const direction of sequence) {
-        switch (direction) {
-            case 'L':
-                x -= scalingFactor;
-                break;
-            case 'R':
-                x += scalingFactor;
-                break;
-        }
-        ctx.lineTo(x, y);
+  for (let i = 0; i < dragonCurve.length; i++) {
+    if (dragonCurve[i]) {
+      direction = (direction + 1) % directions.length;
+    } else {
+      direction = (direction - 1 + directions.length) % directions.length;
     }
+    x += directions[direction][0];
+    y += directions[direction][1];
+    minX = Math.min(minX, x);
+    maxX = Math.max(maxX, x);
+    minY = Math.min(minY, y);
+    maxY = Math.max(maxY, y);
+  }
 
+  let width = maxX - minX + 1,
+    height = maxY - minY + 1,
+    scale = Math.min(size / width, size / height);
+
+  x = -minX * scale + (size - width * scale) / 2;
+  y = -minY * scale + (size - height * scale) / 2;
+  direction = 0;
+
+  let canvas = document.createElement("canvas");
+  canvas.width = size;
+  canvas.height = size;
+
+  let ctx = canvas.getContext("2d");
+
+  // Set the background color to black
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, size, size);
+
+  // Set the stroke color to a green tone
+  ctx.strokeStyle = "rgb(0,128,0)";
+
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+
+  for (let i = 0; i < dragonCurve.length; i++) {
+    if (dragonCurve[i]) {
+      direction = (direction + 1) % directions.length;
+    } else {
+      direction = (direction - 1 + directions.length) % directions.length;
+    }
+    x += directions[direction][0] * scale;
+    y += directions[direction][1] * scale;
+
+    // Change the stroke color to a different green tone
+    ctx.strokeStyle = `rgb(0, ${Math.floor(128 + Math.random() * 128)},0)`;
+
+    ctx.lineTo(x, y);
+
+    // Stroke the current segment
     ctx.stroke();
+
+    // Begin a new path for the next segment
+    ctx.beginPath();
+    ctx.moveTo(x,y);
 }
+
 
 document.getElementById('mintButton').addEventListener('click', mintArt);
 document.getElementById('viewButton').addEventListener('click', () => {
